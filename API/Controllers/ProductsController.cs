614f68e09f3986d45ed07a785415c6375d6fbc9c
Ahmed Maher <45319458+AhmedMaherAli@users.noreply.github.com>
2022-01-17 20:34:20 +0200

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using Infrastructure.Data;
+using Core.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class ProductsController : ControllerBase
+    {
+        private readonly MarketDbContext _marketDbContext;
+
+        public ProductsController(MarketDbContext marketDbContext)
+        {
+            _marketDbContext = marketDbContext;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<Product>>> GetAll()
+        {
+            List<Product>products= await _marketDbContext.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Product>> Get(int id)
+        {
+            Product product = await _marketDbContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+            if (product == null)
+            {
+                return BadRequest("Invalid id.");
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ Core/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Code { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+    }
+}
